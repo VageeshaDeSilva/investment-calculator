c3cc4d9a620cd26a17f4b58d863fef3e6e6c33c2
VageeshaDeSilva <suvadesilva@gmail.com>
2026-04-23 20:09:27 +0530

    add results table to display calculated investment data

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -18,6 +18,38 @@ export default function Results({
       <p>Annual Investment: {calculatedResults.interest}</p>
       <p>Expected Return: {calculatedResults.valueEndOfYear}</p>
       <p>Duration: {calculatedResults.annualInvestment}</p> */}
+      <table className="table-auto border-collapse border border-gray-400">
+        <thead>
+          <tr>
+            <th className="border border-gray-400 px-4 py-2">Year</th>
+            <th className="border border-gray-400 px-4 py-2">Interest</th>
+            <th className="border border-gray-400 px-4 py-2">
+              Value at End of the Year
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Annual Investment
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {calculatedResults.map((results) => (
+            <tr key={results.year}>
+              <td className="border border-gray-400 px-4 py-2">
+                {results.year}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {results.interest}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {results.valueEndOfYear}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {results.annualInvestment}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
